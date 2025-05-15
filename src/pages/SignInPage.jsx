235c502e08de23e2add37761b3d8a3ead391d613
Moldovia-Peach <hughes.mollyann@outlook.com>
2025-05-15 14:33:58 +0100

    Implemented authentication context and signin

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,3 +1,48 @@
+import { useState } from "react";
+import { supabase } from "../supabaseClient";
+import { useNavigate } from "react-router-dom";
+
 export function SignInPage() {
-  return <h1>Hello from signin page</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      setError("Login failed. Please check your credentials.");
+    } else {
+      setError(null);
+      navigate("/dashboard");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Staff Login</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSignIn}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 }
